ad7176de7ed7e30e34f8bf56647597b37ed4864e
DawiddPukaa <Piratsi1>
2023-04-12 10:55:42 +0200

    file uploader with db

--- application/server/src/main/java/pl/kul/blog/SpringBootApplicationLauncher.java
+++ application/server/src/main/java/pl/kul/blog/SpringBootApplicationLauncher.java
@@ -1,28 +1,15 @@
 package pl.kul.blog;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import pl.kul.blog.uploadfilesmodule.FilesStorageService;
-
-import javax.annotation.Resource;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class SpringBootApplicationLauncher implements CommandLineRunner {
-
-    @Resource
-    FilesStorageService storageService;
+public class SpringBootApplicationLauncher {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApplicationLauncher.class, args);
-    }
-
-    @Override
-    public void run(String... arg) throws Exception {
-//    storageService.deleteAll();
-        storageService.init();
     }
 
 }
